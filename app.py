10a19d97f6010eee03126a69f12d62bf5a00b173
20155 <amrhossam8080@gmail.com>
2024-11-26 20:15:47 +0200

    Drocker

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from app import create_app
 import os
 
-
 app = create_app()
+
+# Configurations
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('POSTGRES_USER', 'user')}:{os.getenv('POSTGRES_PASSWORD', 'password')}@db:5432/{os.getenv('POSTGRES_DB', 'flaskdb')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'default-secret-key')
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 5001)
-
-
+    app.run(debug=True, port=5001)
